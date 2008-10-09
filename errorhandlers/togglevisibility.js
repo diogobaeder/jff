a7c4a59e7754371277af184eae9de587f60ff538
Diogo Baeder <diogobaeder@yahoo.com.br>
2008-10-09 20:43:00 +0000

    Refactored ToggleVisibility handler

--- errorhandlers/togglevisibility.js
+++ errorhandlers/togglevisibility.js
@@ -4,34 +4,19 @@
 
 jFF.errorhandlers.ToggleVisibility = function(options) {
     var objRef = this;
-    this.toShow = options[0];
-    this.toHide = options[1];
+    this.objects = options[0];
     this.errorVisible = false;
     
     this.show = function(subject) {
         if (!objRef.errorVisible) {
-            if (objRef.toShow.length) {
-                objRef.toShow.forEach(function(element){
-                    element.show();
-                });
-            }
-            else if(objRef.toShow) {
-                objRef.toShow.show();
-            }
+            objRef.objects.show();
             objRef.errorVisible = true;
         }
     };
     
     this.hide = function(subject) {
         if (objRef.errorVisible) {
-            if (objRef.toHide.length) {
-                objRef.toHide.forEach(function(element){
-                    element.hide();
-                });
-            }
-            else if(objRef.toHide) {
-                objRef.toHide.hide();
-            }
+            objRef.objects.hide();
             objRef.errorVisible = false;
         }
     };
