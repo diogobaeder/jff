0c29e9a221ff6e855b848a57097490cf73c2b00c
Diogo Baeder <diogobaeder@yahoo.com.br>
2008-10-18 09:47:53 +0000

    Added ToggleVisibility test (not stable yet)

--- core/jff.pack.js
+++ core/jff.pack.js
@@ -749,17 +749,20 @@ jFF.errorhandlers.Custom = function(options) {
 jFF.errorhandlers.ToggleVisibility = function(options) {
     var objRef = this;
     this.objects = options[0];
+    this.onlyShowOrHide = options[1];
     this.errorVisible = false;
     
     this.show = function(subject) {
-        if (!objRef.errorVisible) {
+      console.debug('show - '+objRef.onlyShowOrHide);
+        if (!objRef.errorVisible && objRef.onlyShowOrHide != 'only_hide') {
             objRef.objects.show();
             objRef.errorVisible = true;
         }
     };
     
     this.hide = function(subject) {
-        if (objRef.errorVisible) {
+      console.debug('hide - '+objRef.onlyShowOrHide);
+        if (objRef.errorVisible && objRef.onlyShowOrHide != 'only_show') {
             objRef.objects.hide();
             objRef.errorVisible = false;
         }
--- errorhandlers/togglevisibility.js
+++ errorhandlers/togglevisibility.js
@@ -5,17 +5,18 @@
 jFF.errorhandlers.ToggleVisibility = function(options) {
     var objRef = this;
     this.objects = options[0];
+    this.onlyShowOrHide = options[1];
     this.errorVisible = false;
     
     this.show = function(subject) {
-        if (!objRef.errorVisible) {
+        if (!objRef.errorVisible && objRef.onlyShowOrHide != 'only_hide') {
             objRef.objects.show();
             objRef.errorVisible = true;
         }
     };
     
     this.hide = function(subject) {
-        if (objRef.errorVisible) {
+        if (objRef.errorVisible && objRef.onlyShowOrHide != 'only_show') {
             objRef.objects.hide();
             objRef.errorVisible = false;
         }
